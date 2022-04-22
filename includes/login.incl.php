37de3328d43be923484abc795158e2af1c7b1826
Torge Heinsen <torge.heinsen@gmail.com>
2022-04-23 01:57:22 +0200

    change and split login.php, php-part

---
+++ includes/login.incl.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+error_reporting(E_ALL);
+ini_set('display_errors','On');
+require_once('db_config.php');
+if(isset($_POST['absenden']))
+{
+  $benutzername = mysqli_real_escape_string($db, trim($_POST['benutzername']));
+  $passwort = mysqli_real_escape_string($db, trim($_POST['passwort']));
+        $sql="SELECT id, benutzername FROM users WHERE benutzername ='$benutzername' &&     passwort = md5('$passwort')";
+        $data = mysqli_query($db, $sql);
+  if(mysqli_num_rows($data) == 1){
+     $zeile= mysqli_fetch_array($data);
+     $_SESSION['id'] = $zeile['id'];
+     $_SESSION['benutzername'] = $zeile['benutzername'];
+     $_SESSION['angemeldet'] = true;
+        }
+    }
+mysqli_close($db);
+header("location: ../index.php");
+?>
