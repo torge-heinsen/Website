a70f82c5e56deaf77a691c02156bc694f0e88f1a
Torge Heinsen <torge.heinsen@gmail.com>
2022-04-23 01:55:20 +0200

    edited index.php

--- index.php
+++ index.php
@@ -1,28 +1,12 @@
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Startseite</title>
-  </head>
-  <body>
 <?php
-//   session_start();
-if($_GET['page']!=='')
-  $page = $_GET['page'];
-
-if(isset($_SESSION['user'])):
-require_once('ui.php');
-else:
-  if($page == 'login2'):
-    echo 'Zum <a href="index.php?page=registrieren2">registrieren</a> hier entlang';
-    require_once('login2.php');
-  elseif($page == 'registrieren2'):
-    echo 'Zum <a href="index.php?page=login2">login</a> hier entlang';
-    require_once('registrieren2.php');
-  else:
-    echo '<a href="index.php?page=registrieren2">registrieren</a> oder <a href="index.php?page=login">einloggen</a>';
-  endif;
-
-endif;
+include_once('header.php');
+  if(isset($_SESSION['benutzername'])){
+    echo "hallo " . $_SESSION['benutzername'];
+}
+exit();
 ?>
+
+
+
   </body>
 </html>
