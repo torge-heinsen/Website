c02e168f8b1663cc7a1eef0540770154be5cc2ec
Torge Heinsen <torge.heinsen@gmail.com>
2022-04-24 02:41:38 +0200

    also some small changes for members.php

--- includes/login.incl.php
+++ includes/login.incl.php
@@ -7,15 +7,15 @@ if(isset($_POST['absenden']))
 {
   $benutzername = mysqli_real_escape_string($db, trim($_POST['benutzername']));
   $passwort = mysqli_real_escape_string($db, trim($_POST['passwort']));
-        $sql="SELECT id, benutzername FROM users WHERE benutzername ='$benutzername' &&     passwort = md5('$passwort')";
+        $sql="SELECT id, benutzername, rang FROM users WHERE benutzername ='$benutzername' &&     passwort = md5('$passwort')";
         $data = mysqli_query($db, $sql);
   if(mysqli_num_rows($data) == 1){
      $zeile= mysqli_fetch_array($data);
      $_SESSION['id'] = $zeile['id'];
      $_SESSION['benutzername'] = $zeile['benutzername'];
+     $_SESSION['rang'] = $zeile['rang'];
      $_SESSION['angemeldet'] = true;
         }
     }
 mysqli_close($db);
-header("location: ../index.php");
-?>
+header('Location: ' . $start);?>
