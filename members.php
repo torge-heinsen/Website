e1db31005865671a6a1251b2200a0a416eecf177
Torge Heinsen <torge.heinsen@gmail.com>
2022-04-26 00:05:36 +0200

    changed way of data output and added edit button

--- members.php
+++ members.php
@@ -2,26 +2,24 @@
 ob_start();
 include_once('header.php');
 require_once('includes/db_config.php');
-if(isset($_SESSION['benutzername']) && $_SESSION['rang'] == 'admin') {
-          $sql="SELECT id, benutzername, rang FROM users";
-      $data = mysqli_query($db, $sql);
-    echo '<table border="1">';
-    while ($zeile = mysqli_fetch_array($data, MYSQLI_ASSOC))
+if(isset($_SESSION['benutzername']) && $_SESSION['rang'] == 'admin')
+  {
+  $sql="SELECT id, benutzername, rang FROM users";
+  $data = mysqli_query($db, $sql) or exit("sql-error: $sql");
+  echo '<table border="1">';
+    while ($zeile = mysqli_fetch_row($data))
     {
-      echo "<tr>";
-      echo "<td>". $zeile['id'] . "</td>";
-      echo "<td>". $zeile['benutzername'] . "</td>";
-      echo "<td>". $zeile['rang'] . "</td>";
-      echo "</tr>";
+    echo "<tr>";
+      foreach ($zeile as $item)
+        echo "<td>$item</td>";
+        echo "<td><a href=\"includes/member_edit_form.incl.php?id=$zeile[0]\">bearbeiten</td>";
+    echo "</tr>";
     }
-    echo "</table>";
-
+  echo "</table>";
   mysqli_close($db);
-
-
-}
-else{
-    header('Location: ' . $start);
-    exit;
-}
+  }
+  else{
+      header('Location: ' . $start);
+      exit;
+  }
 ob_flush();
