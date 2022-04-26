8e2ad83b9cb157673c84f3affd81521cf1ff9c19
Torge Heinsen <torge.heinsen@gmail.com>
2022-04-27 01:06:05 +0200

    replaced code by function of functions.php

--- members.php
+++ members.php
@@ -2,21 +2,10 @@
 ob_start();
 include_once('header.php');
 require_once('includes/db_config.php');
+require_once('includes/functions.php');
 if(isset($_SESSION['benutzername']) && $_SESSION['rang'] == 'admin')
   {
-  $sql="SELECT id, benutzername, rang FROM users";
-  $data = mysqli_query($db, $sql) or exit("sql-error: $sql");
-  echo '<table border="1">';
-    while ($zeile = mysqli_fetch_row($data))
-    {
-    echo "<tr>";
-      foreach ($zeile as $item)
-        echo "<td>$item</td>";
-        echo "<td><a href=\"includes/member_edit_form.incl.php?id=$zeile[0]\">bearbeiten</td>";
-    echo "</tr>";
-    }
-  echo "</table>";
-  mysqli_close($db);
+  user_query($db);
   }
   else{
       header('Location: ' . $start);
