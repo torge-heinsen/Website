7e4fe5e570b12015ec8fd22f976e009160403416
Torge Heinsen <torge.heinsen@gmail.com>
2022-04-28 02:50:04 +0200

    added page for user overview

---
+++ account.php
@@ -0,0 +1,7 @@
+<?php
+include_once('header.php');
+?>
+<?php //Formular(Button), um sein Passwort zu ändern ?>
+<form action="account_change_settings.php" method="post">
+        <input type="submit" name="change_pass" value="Passwort ändern">
+</form>
