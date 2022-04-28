ec582b78001a96a0a50fadfb5e876e3e543eb0fd
Torge Heinsen <torge.heinsen@gmail.com>
2022-04-29 00:30:21 +0200

    added case sensetive actions(deactivate, activate, delete)

--- includes/member_edit.incl.php
+++ includes/member_edit.incl.php
@@ -4,15 +4,36 @@ require_once('includes/db_config.php');
 require_once('includes/functions.php');
 if(isset($_SESSION['benutzername']) && $_SESSION['rang'] == 'admin')
   {
-    if (isset($_POST['do_edit']))
-    {
-      $sql="UPDATE users SET benutzername='$_POST[benutzername]', rang='$_POST[rang]' WHERE id=$_POST[id]";
+    if(isset($_POST['do_edit']))
+      {
+      $benutzername = mysqli_real_escape_string($db, trim($_POST['benutzername']));
+      $rang = mysqli_real_escape_string($db, trim($_POST['rang']));
+      if (!empty($benutzername) && !empty($rang))
+        {
+        $sql="UPDATE users SET benutzername='$_POST[benutzername]', rang='$_POST[rang]' WHERE id=$_POST[id]";
+        mysqli_query($db, $sql) or exit("sql-error: $sql");
+        }
+      }
+    elseif(isset($_POST['deactivate']))
+      {
+      $sql="UPDATE users SET status='passiv' WHERE id=$_POST[id]";
       mysqli_query($db, $sql) or exit("sql-error: $sql");
-    }
+      }
+    elseif(isset($_POST['activate']))
+      {
+      $sql="UPDATE users SET status='aktiv' WHERE id=$_POST[id]";
+      mysqli_query($db, $sql) or exit("sql-error: $sql");
+      }
+    elseif($_POST['delete'])
+      {
+      $sql="DELETE FROM users WHERE users . id = $_POST[id]";
+      mysqli_query($db, $sql) or exit("sql-error: $sql");
+      }
+
   user_query($db);
   }
-    else
-      {
-      header('Location: ' . $start);
-      exit;
-      }
+  else
+    {
+    header('Location: ' . $start);
+    exit;
+    }
