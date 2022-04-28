5fcd1eb54ae010c76e8326cb34e0b0f2f97aefbe
Torge Heinsen <torge.heinsen@gmail.com>
2022-04-29 00:29:23 +0200

    added case sensetive buttons (activate, deactivate) and the option to delete an user

--- includes/functions.php
+++ includes/functions.php
@@ -1,4 +1,5 @@
 <?php
+//Abfrage aller Nutzer der Seite mit der Möglichkeit diese zu bearbeiten, zu de-/aktivieren oder zu löschen(nur admin)
 function user_query($db){
   $sql="SELECT id, benutzername, rang FROM users";
   $data = mysqli_query($db, $sql) or exit("sql-error: $sql");
@@ -9,7 +10,28 @@ function user_query($db){
       foreach ($zeile as $item)
         echo "<td>$item</td>";
         echo "<td><a href=\"includes/member_edit_form.incl.php?id=$zeile[0]\">bearbeiten</td>";
-    echo "</tr>";
+
+        switch ($zeile[3])
+          {
+          case 'aktiv':
+            echo "<td><form action=\"../includes/member_edit.incl.php\" method=\"post\">";
+            echo "<input type=\"hidden\" name=\"id\" value=\"$zeile[0]\"/>";
+            echo "<input type=\"submit\" name=\"deactivate\" value=\"deaktivieren\">";
+            echo "</form></td>";
+            break;
+          case 'passiv':
+            echo "<td><form action=\"../includes/member_edit.incl.php\" method=\"post\">";
+            echo "<input type=\"hidden\" name=\"id\" value=\"$zeile[0]\"/>";
+            echo "<input type=\"submit\" name=\"activate\" value=\"aktivieren\">";
+            echo "</form></td>";
+            break;
+          }
+
+        echo "<td><form action=\"../includes/member_edit.incl.php\" method=\"post\">";
+        echo "<input type=\"hidden\" name=\"id\" value=\"$zeile[0]\"/>";
+        echo "<input type=\"submit\" name=\"delete\" value=\"löschen\">";
+        echo "</form></td>";
+        echo "</tr>";
     }
   echo "</table>";
   mysqli_close($db);
