552ec6e7a166b4eb16c01d0937c860ee136accf5
Torge Heinsen <torge.heinsen@gmail.com>
2022-04-29 17:13:57 +0200

    added session status, if status(passiv(banned)) they get logged out after redirect to index

--- includes/login.incl.php
+++ includes/login.incl.php
@@ -3,19 +3,24 @@ session_start();
 error_reporting(E_ALL);
 ini_set('display_errors','On');
 require_once('db_config.php');
+//Nach Absenden des Formulars werden die eingebenen Daten mit denen aus der Datebank verglichen,
+//sofern diese übereinstimmen ist der Nutzer eingeloggt
 if(isset($_POST['absenden']))
 {
   $benutzername = mysqli_real_escape_string($db, trim($_POST['benutzername']));
   $passwort = mysqli_real_escape_string($db, trim($_POST['passwort']));
         $sql="SELECT id, benutzername, rang FROM users WHERE benutzername ='$benutzername' &&     passwort = md5('$passwort')";
         $data = mysqli_query($db, $sql);
-  if(mysqli_num_rows($data) == 1){
-     $zeile= mysqli_fetch_array($data);
-     $_SESSION['id'] = $zeile['id'];
-     $_SESSION['benutzername'] = $zeile['benutzername'];
-     $_SESSION['rang'] = $zeile['rang'];
-     $_SESSION['angemeldet'] = true;
-        }
-    }
+      if(mysqli_num_rows($data) == 1)
+          {
+          $zeile= mysqli_fetch_array($data);
+          //Daten werden der aktuellen Session hizugefügt
+          $_SESSION['id'] = $zeile[0];
+          $_SESSION['benutzername'] = $zeile[1];
+          $_SESSION['rang'] = $zeile[2];
+          $_SESSION['status'] = $zeile[3];
+          }
+}
 mysqli_close($db);
-header('Location: ' . $start);?>
+//Nach erfolgreichem einloggen wird der Nutzer auf die Startseite weitergeleitet
+header('Location: ' . $start);
