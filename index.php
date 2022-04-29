363d59a6fbc6484314ac11cdaa6e630a98bd5349
Torge Heinsen <torge.heinsen@gmail.com>
2022-04-29 17:11:14 +0200

    banned users can't log in anymore, until they get activated again

--- index.php
+++ index.php
@@ -1,7 +1,22 @@
 <?php
 include_once('header.php');
-  if(isset($_SESSION['benutzername'])){
-    echo "hallo " . $_SESSION['benutzername'];
+if(isset($_SESSION['status']))
+{
+  if($_SESSION['status']=='passiv')
+    {
+    echo "Du bist gesperrt";
+    session_unset();
+    session_destroy();
+    }
+
+
+  elseif($_SESSION['status']=='aktiv')
+      {
+        if(isset($_SESSION['benutzername']))
+          {
+          echo "hallo " . $_SESSION['benutzername'];
+          }
+      }
 }
 exit();
 ?>
